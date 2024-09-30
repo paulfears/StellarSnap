07570346d205bd8146939a13f1e9a19664801c0b
Paul Fears <paulrfears@gmail.com>
2024-09-30 10:21:11 -0500

    vite snapId development or production id switch

--- site/src/constants.ts
+++ site/src/constants.ts
@@ -1,2 +1,8 @@
-//export const snapId = "npm:stellar-snap"
-export const snapId = "local:http://localhost:8080/"
+
+const isDev = import.meta.env.DEV
+let snapId = "npm:stellar-snap"
+if(isDev){
+    snapId = "local:http://localhost:8080/"
+}
+
+module.exports = {snapId};
